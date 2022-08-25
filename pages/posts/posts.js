28a0677c7f816ec1aa3a7c5bbf9384c9d058057b
Anthony <anthonymarques@outlook.com>
2022-08-25 11:58:31 +0200

    dynamic routes

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -2,9 +2,12 @@ import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
 
+import { remark } from 'remark';
+import html from 'remark-html';
+
 const postsDirectory = path.join(process.cwd(), 'posts')
 
-export function getSortedPostsData() {
+export async function getSortedPostsData() {
   // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory)
   const allPostsData = fileNames.map(fileName => {
@@ -18,10 +21,17 @@ export function getSortedPostsData() {
     // Use gray-matter to parse the post metadata section
     const matterResult = matter(fileContents)
 
+    // Use remark to convert markdown into HTML string
+    const processedContent = await remark()
+      .use(html)
+      .process(matterResult.content);
+    const contentHtml = processedContent.toString();
+
     // Combine the data with the id
     return {
       id,
-      ...matterResult.data
+      contentHtml,
+      ...matterResult.data,
     }
   })
   // Sort posts by date
