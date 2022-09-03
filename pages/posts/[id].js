d6ccc6dbe8a2e1a563a80be802c51c4ed59c838e
Anthony <anthonymarques@outlook.com>
2022-09-03 12:03:23 +0200

    dynamic routing with static content

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,35 +1,45 @@
 import Layout from '../../components/layout';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
+import Head from 'next/head';
+
 export default function Post({ postData }) {
-    return (
-      <Layout>
-        {postData.title}
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
         <br />
-        {postData.id}
-        <br />
-        {postData.date}
-      </Layout>
-    );
-  }
-  
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+
 
 export async function getStaticPaths() {
-    // Return a list of possible value for id
-    const paths = getAllPostIds();
-    return {
-        paths,
-        fallback: false,
-    };
+  // Return a list of possible value for id
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
 
 }
 
 export async function getStaticProps({ params }) {
-    // Fetch necessary data for the blog post using params.id
-    const postData = getPostData(params.id);
-    return {
-      props: {
-        postData,
-      },
-    };
-  }
+  // Fetch necessary data for the blog post using params.id
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
